12a0d40996042c5586e9c8f67a1eb8d22dc920ca
akhilapoosaa <akhila.1829@gmail.com>
2025-11-18 12:47:36 +0530

    updated

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "vpc_cidr"{
+    type    = string
+    description = "please provide vpc cidr"
+}
+
+variable "project_name"{
+    type = string
+}
+
+variable "environment"{
+    type = string
+}
