16ee3010ceef4f1d9f88fa9bb960f7612f6bad20
akhilapoosaa <akhila.1829@gmail.com>
2025-11-18 12:59:04 +0530

    updated

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,10 @@
 resource "aws_vpc" "main"{
     cidr_block  = "10.0.0.0/16"
     instance_tenancy = "default"
+    enable_dns_hostnames = true
 
     tags = merge(   #merge() takes two or more maps and combines them into one final map.
+        var.vpc_tags,   
         local.common_tags,
         {
             Name = local.common_name_suffix
